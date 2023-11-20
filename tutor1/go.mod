a68e3dea375216dcda8b49605817a359aba38854
1xe1 <641463021@crru.ac.th>
2023-11-20 22:42:24 +0700

    a

--- tutor1/go.mod
+++ tutor1/go.mod
@@ -3,5 +3,3 @@ module github.com/1xe1/go-network/tutor1
 go 1.21.4
 
 replace github.com/1xe1/go-network/reverse => ../reverse
-
-require github.com/1xe1/go-network/reverse v0.0.0-00010101000000-000000000000
--- tutor1/hello.go
+++ tutor1/hello.go
@@ -1,16 +1,50 @@
 package main
 
-import (
-	"fmt"
-	"github.com/1xe1/go-network/reverse"
-)
+import "fmt"
 
 func main() {
-	var t bool = true
-	var f bool
-	r := reverse.Reverse("Hello, world!")
+	// ตัวแปรชนิด int
+	var age int
+	age = 25
+	fmt.Println("Age:", age)
 
-	fmt.Println(t)
-	fmt.Println(f)
-	fmt.Println(r)
+	// ตัวแปรชนิด float64
+	var height float64
+	height = 175.5
+	fmt.Println("Height:", height)
+
+	// ตัวแปรชนิด string
+	var name string
+	name = "surasak"
+	fmt.Println("Name:", name)
+
+	// ใช้ := สำหรับการประกาศและกำหนดค่าในทำเลทีฟ (implicit)
+	weight := 68.5
+	isStudent := true
+	fmt.Println("Weight:", weight)
+	fmt.Println("Is Student:", isStudent)
+
+	// ตัวแปรชนิด complex
+	var complexNum complex128
+	complexNum = 3 + 4i
+	fmt.Println("Complex Number:", complexNum)
+
+	// ตัวแปรชนิด array
+	var numbers [3]int
+	numbers[0] = 1
+	numbers[1] = 2
+	numbers[2] = 3
+	fmt.Println("Numbers Array:", numbers)
+
+	// ตัวแปรชนิด slice
+	sliceNumbers := []int{1, 2, 3, 4, 5}
+	fmt.Println("Slice of Numbers:", sliceNumbers)
+
+	// ตัวแปรชนิด map
+	person := map[string]interface{}{
+		"name":   "Alice",
+		"age":    25,
+		"height": 175.0,
+	}
+	fmt.Println("Person Map:", person)
 }
