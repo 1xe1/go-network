0a810ee13d8814ce81a37982ced45f4cb405e985
1xe1 <641463021@crru.ac.th>
2024-01-09 14:06:32 +0700

    update template

--- go-web/main.go
+++ go-web/main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
-    "fmt"
+    "html/template"
     "net/http"
-    "github.com/gorilla/mux"
 )
 
+
+
+type Todo struct {
+    Title string
+    Done  bool
+}
+
+
+
+type TodoPageData struct {
+    PageTitle string
+    Todos     []Todo
+}
+
+
+
 func main() {
-    r := mux.NewRouter()
-
-    r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-        vars := mux.Vars(r)
-        title := vars["title"]
-        page := vars["page"]
-
-        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+    tmpl := template.Must(template.ParseFiles("layout.html"))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        data := TodoPageData{
+            PageTitle: "My TODO list",
+            Todos: []Todo{
+                {Title: "Task 1", Done: false},
+                {Title: "Task 2", Done: true},
+                {Title: "Task 3", Done: true},
+            },
+        }
+        tmpl.Execute(w, data)
     })
-
-    http.ListenAndServe(":80", r)
+    http.ListenAndServe(":80", nil)
 }
