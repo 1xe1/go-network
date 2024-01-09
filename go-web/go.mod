0a63c1d1f21e0e30b3b219f5a7ee18a6984fd6a2
1xe1 <641463021@crru.ac.th>
2024-01-09 13:53:36 +0700

    new

--- go-web/go.mod
+++ go-web/go.mod
@@ -1,3 +1,5 @@
 module github.com/1xe1/go-network/go-web
 
 go 1.21.4
+
+require github.com/gorilla/mux v1.8.1
--- go-web/main.go
+++ go-web/main.go
@@ -3,16 +3,19 @@ package main
 import (
     "fmt"
     "net/http"
+    "github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    r := mux.NewRouter()
+
+    r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+        page := vars["page"]
+
+        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
     })
 
-	fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	fmt.Println("server running...")
-    http.ListenAndServe(":80", nil)
+    http.ListenAndServe(":80", r)
 }
