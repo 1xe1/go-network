3145afa76db079efc2a31079a80a45a08c8e14a5
1xe1 <641463021@crru.ac.th>
2024-01-09 14:21:24 +0700

    static

--- go-web/main.go
+++ go-web/main.go
@@ -1,38 +1,14 @@
 package main
 
 import (
-    "html/template"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
-
-
-type Todo struct {
-    Title string
-    Done  bool
-}
-
-
-
-type TodoPageData struct {
-    PageTitle string
-    Todos     []Todo
-}
-
-
-
 func main() {
-    tmpl := template.Must(template.ParseFiles("layout.html"))
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        data := TodoPageData{
-            PageTitle: "My TODO list",
-            Todos: []Todo{
-                {Title: "Task 1", Done: false},
-                {Title: "Task 2", Done: true},
-                {Title: "Task 3", Done: true},
-            },
-        }
-        tmpl.Execute(w, data)
-    })
+    fs := http.FileServer(http.Dir("assets/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    fmt.Println("server running...")
     http.ListenAndServe(":80", nil)
 }
